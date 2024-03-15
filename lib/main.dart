16d9825a702943cb8123cb4d6e49a665381003f3
Nishit <nishitchaudhary706@gmail.com>
2024-03-15 17:33:12 +0530

    UI Update: Improved the 'Add Cloth' and 'Chat' buttons

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -50,7 +52,26 @@ class _MyHomePageState extends State<MyHomePage> {
                 // Add your logic for handling "Add Cloth" button press
                 // Navigate to the screen for adding cloth or perform related actions
               },
-              child: const Text('Add Cloth'),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.add_circle_outline), // Hanger icon for "Add Cloth"
+                  SizedBox(width: 8), // Add spacing between icon and text
+                  Text(
+                    'Add Cloth',
+                    style: const TextStyle(fontSize: 22),
+                  ),
+                ],
+              ),
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.white, backgroundColor: Colors.blueAccent, // Button text color
+                padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24), // Button padding
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20), // Decreased border radius
+                ),
+                elevation: 4, // Button shadow
+                fixedSize: Size(screenWidth * 0.8, screenWidth*0.2), // Set the minimum width of the button to 80% of the screen width
+              ),
             ),
             const SizedBox(height: 20),
             ElevatedButton(
@@ -61,7 +82,26 @@ class _MyHomePageState extends State<MyHomePage> {
                   MaterialPageRoute(builder: (context) => ChatScreen()),
                 );
               },
-              child: const Text('Chat'),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.message), // Message icon for "Chat"
+                  SizedBox(width: 8), // Add spacing between icon and text
+                  Text(
+                    'Chat',
+                    style: TextStyle(fontSize: 22),
+                  ),
+                ],
+              ),
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.white, backgroundColor: Colors.greenAccent, // Button text color
+                padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24), // Button padding
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20), // Decreased border radius
+                ),
+                elevation: 4, // Button shadow
+                fixedSize: Size(screenWidth * 0.8, screenWidth*0.2), // Set the minimum width of the button to 80% of the screen width
+              ),
             ),
           ],
         ),
