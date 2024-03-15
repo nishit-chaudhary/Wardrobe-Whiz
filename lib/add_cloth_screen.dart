2515b50e36b477522117b0598df59058cf85752d
gittcoder <adityaavinash.n2020@vitstudent.ac.in>
2024-03-15 23:21:40 +0530

    add cloth : process image from camera and gallery

--- lib/add_cloth_screen.dart
+++ lib/add_cloth_screen.dart
@@ -1,237 +1,234 @@
-import 'package:camera/camera.dart';
-import 'package:flutter_tts/flutter_tts.dart';
-import 'package:flutter_vision/flutter_vision.dart';
-import 'package:flutter/material.dart';
+
 import 'dart:async';
+import 'dart:typed_data';
 
-late List<CameraDescription> cameras;
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter_vision/flutter_vision.dart';
+import 'dart:io';
 
-// class AddClothScreen extends StatefulWidget {
-//   @override
-//   _AddClothScreenState createState() => _AddClothScreenState();
-// }
-
-class AddClothScreen extends StatelessWidget {
-
-
+class AddClothScreen extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'MegaView',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurpleAccent),
-          useMaterial3: true,
-        ),
-        debugShowCheckedModeBanner: false,
-        // home: const MainScreen(),
-        home: const YoloVideo()
-    );
-  }
+  _AddClothScreenState createState() => _AddClothScreenState();
 }
 
-// YOLO V5 REAL-TIME OBJECT DETECTION
-
-class YoloVideo extends StatefulWidget {
-  const YoloVideo({super.key});
-
-  @override
-  State<YoloVideo> createState() => _YoloVideoState();
-}
-
-class _YoloVideoState extends State<YoloVideo> {
-  late CameraController controller;
-  late List<Map<String, dynamic>> yoloResults;
-  CameraImage? cameraImage;
-  bool isLoaded = false;
-  bool isDetecting = false;
-
-  late FlutterVision vision; // YOLO
-  FlutterTts flutterTts = FlutterTts(); // TTS
+class _AddClothScreenState extends State<AddClothScreen> {
+  dynamic _image;
+  dynamic _imageHeight;
+  dynamic _imageWidth;
+  final picker = ImagePicker();
+  bool _loading = false;
+  String _description = '';
+  dynamic _recognitions;
+  late FlutterVision vision;
 
   @override
   void initState() {
     super.initState();
-
-    vision = FlutterVision(); // YOLO
-    initTTS(); // TTS
-
-    init();
+    loadModel();
   }
 
-  Future<void> initTTS() async { // TTS
-    await flutterTts.setLanguage("en-US"); // Set the language you want
-    await flutterTts.setSpeechRate(1.0); // Adjust speech rate (1.0 is normal)
-    await flutterTts.setVolume(1.0); // Adjust volume (0.0 to 1.0)
-    await flutterTts.setPitch(1.0); // Adjust pitch (1.0 is normal)
-  }
-
-  Future<void> speak(String text) async {
-    await flutterTts.speak(text); // TTS
-  }
-
-  init() async {
-    cameras = await availableCameras();
-    controller = CameraController(cameras[0], ResolutionPreset.max, enableAudio: false);
-    controller.initialize().then((value) {
-      loadYoloModel().then((value) {
-        setState(() {
-          isLoaded = true;
-          isDetecting = false;
-          yoloResults = [];
-        });
-      });
-    });
-  }
-
-  @override
-  void dispose() async {
-
-    flutterTts.stop(); // TTS Stop
-    vision.closeYoloModel(); // YOLO Stop
-
-    super.dispose();
-    controller.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final Size size = MediaQuery.of(context).size;
-    if (!isLoaded) {
-      return const Scaffold(
-        backgroundColor: Colors.transparent,
-        body: Center(
-          child: Text("Model not loaded. Waiting for it.", style: TextStyle(color: Colors.white)),
-        ),
-      );
-    }
-    return Stack(
-      fit: StackFit.expand,
-      children: [
-        AspectRatio(
-          aspectRatio: controller.value.aspectRatio,
-          child: CameraPreview(
-            controller,
-          ),
-        ),
-        ...displayBoxesAroundRecognizedObjects(size),
-        Positioned(
-          bottom: 75,
-          width: MediaQuery.of(context).size.width,
-          child: Container(
-            height: 80,
-            width: 80,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              border: Border.all(
-                  width: 5, color: Colors.white, style: BorderStyle.solid),
-            ),
-            child: isDetecting
-                ? IconButton(
-              onPressed: () async {
-                stopDetection();
-              },
-              icon: const Icon(
-                Icons.stop,
-                color: Colors.red,
-              ),
-              iconSize: 50,
-            )
-                : IconButton(
-              onPressed: () async {
-                await startDetection();
-              },
-              icon: const Icon(
-                Icons.play_arrow,
-                color: Colors.white,
-              ),
-              iconSize: 50,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Future<void> loadYoloModel() async {
+  Future<void> loadModel() async {
+    vision = FlutterVision();
     await vision.loadYoloModel(
         labels: 'assets/labels.txt',
         modelPath: 'assets/best-fp16.tflite',
         modelVersion: "yolov5",
-        numThreads: 8,
-        useGpu: true);
-    setState(() {
-      isLoaded = true;
-    });
+        quantization: false,
+        numThreads: 1,
+        useGpu: false);
+    print("Model Loaded Successfully!!");
   }
 
-  Future<void> yoloOnFrame(CameraImage cameraImage) async {
-    final result = await vision.yoloOnFrame(
-        bytesList: cameraImage.planes.map((plane) => plane.bytes).toList(),
-        imageHeight: cameraImage.height,
-        imageWidth: cameraImage.width,
-        iouThreshold: 0.4,
+  Future<void> processImage() async {
+    setState(() {
+      _loading = true;
+    });
+
+    // Convert image to byte data
+    print('Hello.....');
+    final Uint8List byte = await _image.readAsBytes();
+    // ByteBuffer buffer = byteData.buffer;
+
+// Create a Uint8List view of the buffer
+//     final Uint8List byte = Uint8List.view(buffer);
+//     final Uint8List byte = await _image.readAsBytes();
+//     final Uint8List byte = byteData.buffer.asUint8List();
+    print('Running Model.....');
+    // Perform inference with TensorFlow Lite model
+    // List? output = await Tflite.runModelOnBinary(
+    //   binary: uint8List,
+    //   asynch: true,
+    // );
+
+    // var recognitions = await Tflite.detectObjectOnImage(
+    //   path: _image.path,
+    //   model: "YOLO",
+    // );
+
+    // setState(() {
+    //   _recognitions = recognitions;
+    // });
+    //
+    // print(recognitions);
+    // ByteBuffer buffer = byteData.buffer;
+
+// Create a Uint8List view of the buffer
+//     Uint8List byte = Uint8List.view(buffer);
+//     Uint8List imgBytes = buffer.asUint8List(byteData.offsetInBytes, byteData.lengthInBytes);
+    final result = await vision.yoloOnImage(
+        bytesList: byte,
+        imageHeight:_imageHeight,
+        imageWidth:_imageWidth,
+        iouThreshold: 0.8,
         confThreshold: 0.4,
-        classThreshold: 0.5);
-    if (result.isNotEmpty) {
-      setState(() {
-        yoloResults = result;
-      });
+        classThreshold: 0.7);
+    print('Lesgoooo!!!');
+    print(result);
+    // Parse output to get description
+    // Replace this with your actual parsing logic
+    _description = 'This is a description of the processed image';
+    if (result.isEmpty) {
+      // Show alert
+
+
+      _showEmptyResultsAlert(context);
     }
+    else
+    {
+      _description ="The given image contains the Following Clothing items :";
+
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ProcessedImageScreen(image: _image, description: _description),
+        ),
+      );
+
+
+    }
+
+
+    setState(() {
+      _loading = false;
+    });
+
+
   }
 
-  Future<void> startDetection() async {
+  Future getImage(ImageSource source) async {
+    final pickedFile = await picker.pickImage(source: source);
+    print("Got Image !!!");
     setState(() {
-      isDetecting = true;
-    });
-    if (controller.value.isStreamingImages) {
-      return;
-    }
-    await controller.startImageStream((image) async {
-      if (isDetecting) {
-        cameraImage = image;
-        yoloOnFrame(image);
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+        final image = Image.file(File(pickedFile.path));
+        image.image.resolve(ImageConfiguration()).addListener(ImageStreamListener(
+              (ImageInfo info, bool _) {
+            print("Storing Size !!!!");
+            _imageWidth=info.image.width;
+            _imageHeight=info.image.height;
+          },
+        ));
+
+
+
+      } else {
+        print('No image selected.');
       }
     });
   }
 
-  Future<void> stopDetection() async {
-    setState(() {
-      isDetecting = false;
-      yoloResults.clear();
-    });
-  }
-
-  List<Widget> displayBoxesAroundRecognizedObjects(Size screen) {
-    if (yoloResults.isEmpty) return [];
-    double factorX = screen.width / (cameraImage?.height ?? 1);
-    double factorY = screen.height / (cameraImage?.width ?? 1);
-
-    Color colorPick = const Color.fromARGB(255, 50, 233, 30);
-
-    return yoloResults.map((result) {
-
-      speak("${result['tag']}");
-
-      return Positioned(
-        left: result["box"][0] * factorX,
-        top: result["box"][1] * factorY,
-        width: (result["box"][2] - result["box"][0]) * factorX,
-        height: (result["box"][3] - result["box"][1]) * factorY,
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: const BorderRadius.all(Radius.circular(10.0)),
-            border: Border.all(color: Colors.pink, width: 2.0),
-          ),
-          child: Text(
-            "${result['tag']} ${(result['box'][4] * 100).toStringAsFixed(0)}%",
-            style: TextStyle(
-              background: Paint()..color = colorPick,
-              color: Colors.white,
-              fontSize: 18.0,
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Cloth'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _image == null
+                ? Text('No image selected.')
+                : Image.file(
+              _image,
+              width: 300,
+              height: 300,
+              fit: BoxFit.cover,
             ),
-          ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: ()=>getImage(ImageSource.camera),
+              child: Text('Take Photo'),
+            ),
+            ElevatedButton(
+                onPressed: () => getImage(ImageSource.gallery),
+                child: Text('Choose from Gallery')),
+            SizedBox(height: 20),
+            _loading
+                ? CircularProgressIndicator() // Show loading indicator if processing
+                : ElevatedButton(
+              onPressed: _image != null ? processImage : null,
+              child: Text('Next'),
+            ),
+          ],
         ),
-      );
-    }).toList();
+      ),
+    );
   }
+}
+
+class ProcessedImageScreen extends StatelessWidget {
+  final File image;
+  final String description;
+
+  ProcessedImageScreen({required this.image, required this.description});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Processed Image'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.file(
+              image,
+              width: 300,
+              height: 300,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Description: $description',
+              style: TextStyle(fontSize: 18),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void _showEmptyResultsAlert(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('No Clothing Detected'),
+        content: Text('Could not detect any clothing. Please try with another picture.'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('OK'),
+          ),
+        ],
+      );
+    },
+  );
 }
