da92423e535ecdb53be2a875052cd1569292a77c
gittcoder <adityaavinash.n2020@vitstudent.ac.in>
2024-04-26 13:28:33 +0530

    chat screen : fixed empty path issue

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -9,25 +9,28 @@ import 'package:speech_to_text/speech_to_text.dart' as stt;
 import 'package:geolocator/geolocator.dart';
 import 'dart:io';
 
+
 class ChatMessage {
   final String text;
   final List<dynamic> clothes;
   final bool isUser;
 
-  ChatMessage(
-      {required this.text, required this.isUser, required this.clothes});
+  ChatMessage({required this.text, required this.isUser,required this.clothes});
 
   Map<String, dynamic> toJson() {
     return {
       'text': text,
       'isUser': isUser,
-      'clothes': clothes,
+      'clothes':clothes,
     };
   }
 
   factory ChatMessage.fromJson(Map<String, dynamic> json) {
     return ChatMessage(
-        text: json['text'], isUser: json['isUser'], clothes: json['clothes']);
+        text: json['text'],
+        isUser: json['isUser'],
+        clothes:json['clothes']
+    );
   }
 }
 
@@ -39,9 +42,9 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-  List<List<String>> _clothes = [];
-  String forecast = "";
-  bool _showimage = false;
+  List<List<String>> _clothes=[];
+  String forecast="";
+  bool _showimage=false;
   stt.SpeechToText _speech = stt.SpeechToText();
   final TextEditingController _messageController = TextEditingController();
   List<ChatMessage> _messages = [];
@@ -64,22 +67,19 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
-  Future<http.Response> sendQuery(
-      String message, String user, String pass) async {
-    final response = await http
-        .post(
-            Uri.parse(_endpoint +
-                "/chat"), // Replace api_endpoint with your API endpoint
-            headers: <String, String>{
-              'Content-Type': 'application/json; charset=UTF-8',
-            },
-            body: jsonEncode({
-              "body":
-                  json.encode({"message": message, "user": user, "pass": pass})
-            }))
-        .timeout(Duration(seconds: 30));
+  Future<http.Response> sendQuery(String message,String user,String pass) async {
+    final response = await http.post(
+        Uri.parse(
+            _endpoint + "/chat"), // Replace api_endpoint with your API endpoint
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode({"body": json.encode({"message":message,"user":user,"pass":pass})})
+    ).timeout(Duration(seconds: 30));
     return response;
   }
+
+
 
   Future<void> _loadEndpoint() async {
     print("Connecting to database !!!");
@@ -116,15 +116,15 @@ class _ChatScreenState extends State<ChatScreen> {
   }
 
   void _sendMessage(String message) async {
-    List<String> clothes = [];
-    _showimage = true;
+    List<String> clothes=[];
+    _showimage=true;
     // Your code to send message and handle response goes here...
     setState(() {
       _isLoading = true;
       // Add the user's message to the chat history
       _messages.add(ChatMessage(
         text: message,
-        clothes: clothes,
+        clothes:clothes,
         isUser: true,
       ));
     });
@@ -134,16 +134,39 @@ class _ChatScreenState extends State<ChatScreen> {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String? username = prefs.getString('username') ?? '';
     String password = prefs.getString('password') ?? '';
-    dynamic response = await sendQuery(message + forecast, username, password);
+    dynamic response = await sendQuery(message+forecast,username,password);
 
-    clothes = List<String>.from(
-        json.decode(response.body.toString())['response'] as List);
+    clothes=List<String>.from(json.decode(response.body.toString())['response'] as List);
+    print(clothes);
+    Set<String> uniquePaths = Set(); // Using a set to avoid duplicates
+    List<String> validPaths = []; // To store valid paths
+
+    for (String path in clothes) {
+      // Check if the path exists (file or directory)
+      bool exists = await (File("/data/user/0/com.example.wardrobe_whiz/app_flutter/savedImages/"+path).exists());
+
+      // Add to the list if it's valid and not a duplicate
+      if (exists && !uniquePaths.contains(path)) {
+        uniquePaths.add(path); // Add to the set to avoid duplicates
+        validPaths.add(path); // Add to the list of valid paths
+      }
+
+    }
+      String resText="Recommended clothes :";
+      if(validPaths.isEmpty)
+        {
+          resText="Sorry, I couldn't find any clothes that match your preferences at the moment. Could you please try again?";
+        }
+
     // Simulate the chatbot's response (replace with actual chatbot response)
     setState(() {
       _isLoading = false;
       print(response.body.toString());
       _messages.add(ChatMessage(
-          text: "Recommended clothes :", isUser: false, clothes: clothes));
+          text: resText,
+          isUser: false,
+          clothes:validPaths
+      ));
     });
 
     // Save the updated chat history to SharedPreferences
@@ -158,7 +181,7 @@ class _ChatScreenState extends State<ChatScreen> {
     // Example code to simulate response from chatbot
   }
 
-  Future<Position> _getLocation() async {
+  Future<Position> _getLocation() async{
     bool serviceEnabled;
     LocationPermission permission;
 
@@ -181,36 +204,31 @@ class _ChatScreenState extends State<ChatScreen> {
           'Location permissions are permanently denied, we cannot request permissions.');
     }
 
-    dynamic loc = await Geolocator.getCurrentPosition();
+    dynamic loc= await Geolocator.getCurrentPosition();
     print("Getting Location!!");
     print(loc);
     return loc;
   }
-
   Future<http.Response> getWeather(String message) async {
     final response = await http.get(
-      Uri.parse(
-          "http://api.weatherapi.com/v1/current.json?key=f2316321459d48e2a5c34518242404&q=" +
-              message +
-              "Bangalore&aqi=no"),
+      Uri.parse("http://api.weatherapi.com/v1/current.json?key=f2316321459d48e2a5c34518242404&q="
+          +message+"Bangalore&aqi=no"),
       // Replace api_endpoint with your API endpoint
     );
     return response;
   }
-
-  void _getForecast() async {
+  void _getForecast() async
+  {
     Position position = await _getLocation();
-    final response = await getWeather(
-        position.latitude.toString() + "," + position.longitude.toString());
-    dynamic x = json.decode(response.body.toString());
+    final response= await getWeather(position.latitude.toString()+","+position.longitude.toString());
+    dynamic x=json.decode(response.body.toString());
     print(x);
-    String f = x["current"]["condition"]["text"];
-    String p = "precipitation:" +
-        x["current"]["precip_mm"].toString() +
-        "huidity:" +
-        x["current"]["humidity"].toString();
+    String f=x["current"]["condition"]["text"];
+    String p="precipitation:"+x["current"]["precip_mm"].toString()+"huidity:"+x["current"]["humidity"].toString();
     // forecast="Weather forecast : "+f+" "+p;
-    forecast = "Weather forecast : " + f;
+    forecast="Weather forecast : "+f;
+
+
   }
 
   @override
@@ -220,9 +238,9 @@ class _ChatScreenState extends State<ChatScreen> {
         actions: [
           IconButton(
             icon: Icon(Icons.delete),
-            onPressed: () async {
+            onPressed: () async{
               SharedPreferences prefs = await SharedPreferences.getInstance();
-              await prefs.setStringList('chat_history', []);
+              await prefs.setStringList('chat_history',[]);
               setState(() {
                 _messages = [];
               });
@@ -244,11 +262,14 @@ class _ChatScreenState extends State<ChatScreen> {
                       vertical: 4.0,
                       horizontal: 8.0,
                     ),
-                    child: Column(children: <Widget>[
-                      ChatBubble(
-                        message: _messages[index],
-                      ),
-                    ]));
+                    child:Column(
+                        children: <Widget>[ ChatBubble(
+                          message: _messages[index],
+                        ),
+
+                        ]
+                    )
+                );
               },
             ),
           ),
@@ -273,36 +294,38 @@ class _ChatScreenState extends State<ChatScreen> {
                   onPressed: _isLoading // Disable button when loading
                       ? null
                       : () {
+                    setState(() {
+                      _isListening = !_isListening;
+                    });
+                    if (_isListening) {
+                      _speech.listen(
+                        onResult: (result) {
                           setState(() {
-                            _isListening = !_isListening;
+                            _messageController.text =
+                                result.recognizedWords;
+
                           });
-                          if (_isListening) {
-                            _speech.listen(
-                              onResult: (result) {
-                                setState(() {
-                                  _messageController.text =
-                                      result.recognizedWords;
-                                });
-                              },
-                            );
-                          } else {
-                            _speech.stop();
-                            if (_messageController.text.isNotEmpty) {
-                              _sendMessage(_messageController.text);
-                            }
-                          }
                         },
+                      );
+                    } else {
+                      _speech.stop();
+                      if (_messageController.text.isNotEmpty) {
+                        _sendMessage(_messageController.text);
+                      }
+                    }
+                  },
                 ),
                 IconButton(
                   icon: Icon(Icons.send),
                   onPressed: _isLoading // Disable button when loading
                       ? null
                       : () {
-                          if (_messageController.text.isNotEmpty) {
-                            _sendMessage(_messageController.text);
-                          }
-                        },
+                    if (_messageController.text.isNotEmpty) {
+                      _sendMessage(_messageController.text);
+                    }
+                  },
                 ),
+
                 if (_isLoading) // Show loading indicator if loading
                   Padding(
                     padding: EdgeInsets.symmetric(horizontal: 16.0),
@@ -333,36 +356,37 @@ class ChatBubble extends StatelessWidget {
         ),
         padding: const EdgeInsets.all(12.0),
         margin: const EdgeInsets.symmetric(vertical: 4.0),
-        child: Column(children: <Widget>[
-          Text(
-            message.text,
-            style: TextStyle(fontSize: 16.0),
-          ),
-          GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 8,
-              mainAxisSpacing: 8,
-            ),
-            shrinkWrap: true,
-            physics: NeverScrollableScrollPhysics(),
-            itemCount: message.clothes.length,
-            itemBuilder: (context, i) {
-              String imagePath = message.clothes[i];
 
-              return GestureDetector(
-                child: Card(
-                  child: Image.file(
-                    File(
-                        "/data/user/0/com.example.wardrobe_whiz/app_flutter/savedImages/" +
-                            imagePath),
-                    fit: BoxFit.cover,
-                  ),
+        child:Column(
+
+            children: <Widget>[ Text(
+              message.text,
+              style: TextStyle(fontSize: 16.0),
+            ),
+              GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 8,
+                  mainAxisSpacing: 8,
                 ),
-              );
-            },
-          ),
-        ]),
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: message.clothes.length,
+                itemBuilder: (context, i) {
+                  String imagePath = message.clothes[i];
+
+                  return GestureDetector(
+                    child: Card(
+                      child: Image.file(
+                        File("/data/user/0/com.example.wardrobe_whiz/app_flutter/savedImages/"+imagePath),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ]
+        ),
       ),
     );
   }
